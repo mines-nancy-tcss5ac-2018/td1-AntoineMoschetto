5bab50bdaf528383e41646ff71b60ad708062c51
AntoineMoschetto <43882662+AntoineMoschetto@users.noreply.github.com>
2018-10-05 16:34:37 +0200

    Add files via upload

---
+++ euler22.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  5 15:39:43 2018
+
+@author: moschett4u
+"""
+
+lis=[]
+for line in open('p022_names.txt', 'r'):
+    lis.append(line)
+    lis=str(lis)
+
+liste=lis.split('","')
+liste[0]='MARY'
+liste[-1]='ALONSO'
+listriee=sorted(liste)
+
+
+
+
+def points(nom):
+    s=0
+    for k in range(len(nom)):
+        s+=ord(nom[k])-64
+    return(s)
+    
+def solve(lis):
+    s=0
+    for k in range(len(lis)):
+        s+=points(lis[k])*(k+1)
+        
+    return(s)
+    
+assert(points('COLIN')==53)
+print("la solution du probleme euler 22 est", solve(listriee))
